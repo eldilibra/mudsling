6e52d62b0ae1e2b6d2dbbd954baa9fde87e60086
663788shadow <663788shadow@users.noreply.github.com>
2014-04-27 21:31:59 -0400

    player model class
    It is a .cpp

---
+++ src/player.cpp
@@ -0,0 +1,13 @@
+#include "player.h"
+#include <iostream>
+using namespace std;
+
+void player :: id(int a)
+
+void player :: username(string b)
+
+void player :: password(string p)
+
+void player :: inventory(string i[c])
+
+
