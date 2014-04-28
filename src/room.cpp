66e1ad49ad04fac52957c67fba945bbc53799386
663788shadow <663788shadow@users.noreply.github.com>
2014-04-27 21:44:38 -0400

    Create room.cpp

---
+++ src/room.cpp
@@ -0,0 +1,13 @@
+#inlcude "room.h"
+using namespace std;
+
+void room :: id_of_room(int i[c])
+
+void room :: item(string a[b])
+
+void room :: current_players(int d[e])
+
+void north_Room(NULL)
+void east_Room(NULL)
+void south_Room(NULL)
+void west_Room(NULL)
