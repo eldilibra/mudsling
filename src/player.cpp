97f212724708f512717e66c0b0ba9d628e48d89d
shadowgeekninja <shadowgeekninja@users.noreply.github.com>
2014-05-11 16:35:00 -0700

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -3,10 +3,10 @@
 Player::Player() {
 }
 
-void Player::id(int a)
+void Player::setid(int a)
 
-void Player::username(string b)
+void Player::setusername(string b)
 
-void Player::password(string p)
+void Player::setpassword(string p)
 
-void Player::inventory(string i[c])
+void Player::setinventory(string i[c])
