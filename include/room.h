2c2803193af3da00e2069f9dbcee0a30fbfd3888
shadowgeekninja <shadowgeekninja@users.noreply.github.com>
2014-05-11 16:38:27 -0700

    Update room.h

--- include/room.h
+++ include/room.h
@@ -1,13 +1,25 @@
 using namespace std;
 class room
-private:
-  void id(int i[c]);
+public:
+  void getid(int i[c]){
+  }
 
-  void item(string a[b]);
+  void getitem(string a[b]){
+    
+  }
 
-  void current_players(int d[e]);
-
-  void north_Room(NULL);
-  void east_Room(NULL);
-  void south_Room(NULL);
-  void west_Room(NULL);
+  void getcurrent_players(int d[e]){
+    
+  }
+g
+  void getnorth_Room(NULL){
+  }
+  void geteast_Room(NULL){
+    
+  }
+  void getsouth_Room(NULL){
+    
+  }
+  void getwest_Room(NULL){
+    
+  }
