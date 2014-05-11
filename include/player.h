6d86cfd9c4e074c1e29a8086ecde993df559eaed
shadowgeekninja <shadowgeekninja@users.noreply.github.com>
2014-05-11 16:30:51 -0700

    Update player.h

--- include/player.h
+++ include/player.h
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 class player
-private:
-  void id(int a);
-  void username(string b);
+public:
+  getid(int a){
+  }
+  getusername(string b){
+    
+  }
 
-  void password(string p);
-
-  void inventory(string i[c]);
+  getpassword(string p){
+    
+  }
+  getinventory(string i[c]){
+    
+  }
 
