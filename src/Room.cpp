80d54792ff7e0f493b2247c408e01c0b9c7e831d
shadowgeekninja <shadowgeekninja@users.noreply.github.com>
2014-05-11 17:56:35 -0700

    Update and rename room.cpp to Room.cpp

--- src/Room.cpp
+++ src/Room.cpp
@@ -1,21 +1,21 @@
-#include "room.h"
+#include "Room.h"
 using namespace std;
 public:
-void room :: setid(int i[c])
+void Room :: setid(int i[c])
 
-void room :: setitem(string a[b])
+void Room :: setitem(string a[b])
 
-void room :: setcurrent_players(int d[e])
+void Room :: setcurrent_players(int d[e])
 void setnorth_Room(NULL)
 void seteast_Room(NULL)
 void setsouth_Room(NULL)
 void setwest_Room(NULL)
 private:
-void room :: setid(int i[c])
+void Room :: setid(int i[c])
 
-void room :: item(string a[b])
+void Room :: item(string a[b])
 
-void room :: current_players(int d[e])
+void Room :: current_players(int d[e])
 
 void north_Room(NULL)
 void east_Room(NULL)
