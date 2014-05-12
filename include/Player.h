2a5e5e4e9eff74c4ad83079b0989e43530d66ec8
shadowgeekninja <shadowgeekninja@users.noreply.github.com>
2014-05-11 17:55:32 -0700

    Update and rename player.h to Player.h

--- include/Player.h
+++ include/Player.h
@@ -1,9 +1,14 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 #include <iostream>
 using namespace std;
-class player
+class Player
 private:
   int a;
   string b,p,i,c;
+  Player()
+  {
+  }
 public:
 getid(int a){
   
