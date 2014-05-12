b14cebc047d6b571221b6821376ac8d80ae70d53
shadowgeekninja <shadowgeekninja@users.noreply.github.com>
2014-05-11 17:01:53 -0700

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -2,7 +2,7 @@
 
 Player::Player() {
 }
-
+public:
 void Player::setid(int a)
 
 void Player::setusername(string b)
@@ -10,3 +10,11 @@ void Player::setusername(string b)
 void Player::setpassword(string p)
 
 void Player::setinventory(string i[c])
+private:
+void Player::id(int a)
+
+void Player::username(string b)
+
+void Player::password(string p)
+
+void Player::inventory(string i[c])
