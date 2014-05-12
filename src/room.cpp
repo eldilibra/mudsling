c6aa4d18d0abea8eccd75dff9f02b5dc799050f5
shadowgeekninja <shadowgeekninja@users.noreply.github.com>
2014-05-11 17:05:05 -0700

    Update room.cpp

--- src/room.cpp
+++ src/room.cpp
@@ -1,24 +1,23 @@
 #include "room.h"
 using namespace std;
 public:
-void room :: setid(int i[c]);
+void room :: setid(int i[c])
 
-void room :: setitem(string a[b]);
+void room :: setitem(string a[b])
 
-void room :: setcurrent_players(int d[e]);
-
-void setnorth_Room(NULL);
-void seteast_Room(NULL);
-void setsouth_Room(NULL);
-void setwest_Room(NULL);
+void room :: setcurrent_players(int d[e])
+void setnorth_Room(NULL)
+void seteast_Room(NULL)
+void setsouth_Room(NULL)
+void setwest_Room(NULL)
 private:
-void room :: setid(int i[c]);
+void room :: setid(int i[c])
 
-void room :: item(string a[b]);
+void room :: item(string a[b])
 
-void room :: current_players(int d[e]);
+void room :: current_players(int d[e])
 
-void north_Room(NULL);
-void east_Room(NULL);
-void south_Room(NULL);
-void west_Room(NULL);
+void north_Room(NULL)
+void east_Room(NULL)
+void south_Room(NULL)
+void west_Room(NULL)
